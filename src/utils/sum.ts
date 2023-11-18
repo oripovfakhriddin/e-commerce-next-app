7e66b36f3ac2a973cb22e120207bece2255d9ba3
oripovfakhriddin <fakhriddinoripov0731@gmail.com>
2023-11-19 00:11:05 +0500

    prettier

--- src/utils/sum.ts
+++ src/utils/sum.ts
@@ -1,17 +1,9 @@
-// const sepreateSumm = (num: number) => {
-//   let money = '';
-//   for(let i=0; i<num.toString().length; i++){
-
-//   }
-//   return money
-// }
-
 export const trueDate = (date: string) => {
-    const berilganVaqt = new Date(date);
-    const sana = berilganVaqt.toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
-    return sana;
-  };
+  const berilganVaqt = new Date(date);
+  const sana = berilganVaqt.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  return sana;
+};
